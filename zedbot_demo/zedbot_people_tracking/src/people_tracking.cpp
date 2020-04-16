948bf2abbe45e855d2b00127da0a33339d6d7191
Louis_38 <louis.valery@ensta-bretagne.org>
2020-04-16 14:09:12 +0200

    .

--- zedbot_demo/zedbot_people_tracking/src/people_tracking.cpp
+++ zedbot_demo/zedbot_people_tracking/src/people_tracking.cpp
@@ -108,32 +108,32 @@ public:
             string arrow_direction = params["arrow_direction"].get<string>();
 
             IoTCloud::logInfo("Arrow direction : " + arrow_direction );
-            // if (m_remote_control_enabled){
-            //     if (arrow_direction == "up"){
-            //         m_command.linear.x =  0.05;
-            //         m_command.angular.z= 0;
-            //         m_command.linear.y = 0;
-            //         m_cmd_vel_pub.publish(m_command);
-            //     }
-            //     else if (arrow_direction == "down"){
-            //         m_command.linear.x =  -0.05;
-            //         m_command.angular.z= 0;
-            //         m_command.linear.y = 0;
-            //         m_cmd_vel_pub.publish(m_command);
-            //     }
-            //     else if (arrow_direction == "left"){
-            //         m_command.linear.x =  0;
-            //         m_command.angular.z= -0.01;
-            //         m_command.linear.y = 0;
-            //         m_cmd_vel_pub.publish(m_command);
-            //     }
-            //     else if (arrow_direction == "right"){
-            //         m_command.linear.x =  0;
-            //         m_command.angular.z= 0.01;
-            //         m_command.linear.y = 0;
-            //         m_cmd_vel_pub.publish(m_command);
-            //     }
-            // }
+            if (m_remote_control_enabled){
+                if (arrow_direction == "up"){
+                    m_command.linear.x =  0.05;
+                    m_command.angular.z= 0;
+                    m_command.linear.y = 0;
+                    m_cmd_vel_pub.publish(m_command);
+                }
+                else if (arrow_direction == "down"){
+                    m_command.linear.x =  -0.05;
+                    m_command.angular.z= 0;
+                    m_command.linear.y = 0;
+                    m_cmd_vel_pub.publish(m_command);
+                }
+                else if (arrow_direction == "left"){
+                    m_command.linear.x =  0;
+                    m_command.angular.z= -0.01;
+                    m_command.linear.y = 0;
+                    m_cmd_vel_pub.publish(m_command);
+                }
+                else if (arrow_direction == "right"){
+                    m_command.linear.x =  0;
+                    m_command.angular.z= 0.01;
+                    m_command.linear.y = 0;
+                    m_cmd_vel_pub.publish(m_command);
+                }
+            }
             //Update the result and status of the event
             event.status = 0;
             event.result = arrow_direction;
@@ -476,11 +476,11 @@ int main(int argc, char** argv) {
     PeopleTracking PeopleTrackingObject;
 
     CallbackParameters arrow_callback_params;
-    arrow_callback_params.setRemoteCallback("arrow_direction_function", CALLBACK_TYPE::ON_REMOTE_CALL, nullptr);
+    arrow_callback_params.setRemoteCallback("arrow_direction_function", CALLBACK_TYPE::ON_REMOTE_CALL, this);
     IoTCloud::registerFunction(PeopleTracking::arrow_cmd_callback, arrow_callback_params);
 
     CallbackParameters allow_remot_control_callback_params;
-    allow_remot_control_callback_params.setRemoteCallback("remote_control_signal_function", CALLBACK_TYPE::ON_REMOTE_CALL, nullptr);
+    allow_remot_control_callback_params.setRemoteCallback("remote_control_signal_function", CALLBACK_TYPE::ON_REMOTE_CALL, this);
     IoTCloud::registerFunction(PeopleTracking::allow_remote_control_callback, allow_remot_control_callback_params);
     IoTCloud::logInfo("Remote functions initialized");
     
